08fc1e7cfdcb9e0a9163f8c14a345f363d398beb
Webgoonie <webgoonie@gmail.com>
2024-08-25 09:13:05 -0400

    Moved old files into new repo

--- Connections/wfh_connection.php
+++ Connections/wfh_connection.php
@@ -8,4 +8,5 @@ $username_wfh_connection = "idsids_wefinance";
 $password_wfh_connection = "yrBIBVwHt)6p";
 
 $wfh_connection_mysqli = mysqli_connect($hostname_wfh_connection, $username_wfh_connection, $password_wfh_connection) or trigger_error(mysql_error(),E_USER_ERROR); 
+
 ?>
